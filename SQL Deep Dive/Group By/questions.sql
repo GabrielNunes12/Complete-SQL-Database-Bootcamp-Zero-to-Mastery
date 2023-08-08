19977b7512ec3acc9a85cfc64f820f250100749a
Gabriel Nunes <48297244+GabrielNunes12@users.noreply.github.com>
2023-08-08 20:00:15 -0300

    Update questions.sql

--- SQL Deep Dive/Group By/questions.sql
+++ SQL Deep Dive/Group By/questions.sql
@@ -3,17 +3,21 @@
 *  Database: Employees
 *  Table: Employees
 */
-
-SELECT e.emp_no
+SELECT count(e.emp_no), e.hire_date 
 FROM employees as e
+group by e.hire_date 
+
 
 /*
 *   Show me all the employees, hired after 1991 and count the amount of positions they've had
 *  Database: Employees
 */
 
-SELECT e.emp_no
+SELECT e.emp_no, count(t.title) as "titles"
 FROM employees as e
+JOIN titles as t USING(emp_no)
+WHERE EXTRACT (YEAR FROM e.hire_date) > 1991
+group by e.emp_no;
 
 
 /*
@@ -21,5 +25,9 @@ FROM employees as e
 *  Database: Employees
 */
 
-SELECT e.emp_no
+SELECT e.emp_no, de.from_date, de.to_date
 FROM employees as e
+JOIN dept_emp AS de USING(emp_no)
+where de.dept_no = 'd005'
+group by e.emp_no, de.from_date, de.to_date
+order by e.emp_no ;
